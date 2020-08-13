21b2afbc530c130ff2b9620174da87cfd96845f8
Brian Cavalier <brian@briancavalier.com>
2020-08-13 08:02:46 -0400

    Fix getLocation type, add npm start

--- src/application/pets.ts
+++ src/application/pets.ts
@@ -2,7 +2,7 @@ import {
   AdoptablePets, GetLocation, GetPets, IPAddress, isIPAddress, Location, Pet
 } from '../domain/model'
 
-export type Env = GetLocation<IPAddress | Location, Location | null> & GetPets<Location, readonly Pet[]>
+export type Env = GetLocation<IPAddress, Location | null> & GetPets<Location, readonly Pet[]>
 
 export const getPetsNear = async ({ getLocation, getPets }: Env, x: IPAddress | Location): Promise<AdoptablePets<Location | null, readonly Pet[]>> => {
   const location = isIPAddress(x) ? await getLocation(x) : x
